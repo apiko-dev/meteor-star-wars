d08db189af2cfb09c5d09d6697d5db83d2995d06
Michael Rokosh <mike.rokosh@gmail.com>
2015-12-28 11:05:06 +0200

    Sight image

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jss:star-wars',
-  version: '1.1.3',
+  version: '1.1.4',
   summary: 'Destroy the web-site with a bunch of weapons from the Star Wars movie',
   git: 'https://github.com/JSSolutions/meteor-star-wars',
   documentation: 'README.md'
